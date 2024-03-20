f4159995b3f01dbdf4a8529d592f7a3081e8a292
Jorkaeff Lainez <33355533+AJLS05@users.noreply.github.com>
2024-03-20 11:27:58 -0600

    agg workflow de gh actions para deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: 'https://AJLS05.github.io',
+  base: '/AJLS05/portfolio.web',
 });
